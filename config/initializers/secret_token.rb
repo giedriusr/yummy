0c4151a559adb15a8863530648627b58e9462a6d
Giedrius Rimkus <info@giedriusr.lt>
2012-02-24 00:10:35 +0200

    initial commit. new rails application, almost empty, just created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodsearch::Application.config.secret_token = '5595880e789c7ced317b33b778463b54b4ae9ed1e8cfc3f93d7ba1e5210b1864bdbb934d5a3c8c3063ebe341c6322a7ba1783445fa47f407791b2dd2581a883a'
