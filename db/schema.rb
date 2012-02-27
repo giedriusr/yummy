d81cf8a59ecc3e1236bbd16c66ab8377bc59a319
diusha13 <diusha13@gmail.com>
2012-02-28 00:30:13 +0200

    Some grids implementation

--- db/schema.rb
+++ db/schema.rb
@@ -25,10 +25,10 @@ ActiveRecord::Schema.define(:version => 20120227162345) do
     t.string   "address"
     t.string   "city"
     t.string   "country_code"
-    t.float    "longitude"
-    t.float    "latitude"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.float    "longitude"
+    t.float    "latitude"
   end
 
   create_table "customers", :force => true do |t|
@@ -59,14 +59,13 @@ ActiveRecord::Schema.define(:version => 20120227162345) do
 
   create_table "ingredients", :force => true do |t|
     t.string   "name"
-    t.float    "price"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
 
   create_table "items", :force => true do |t|
     t.string   "name"
-    t.float    "price"
+    t.decimal  "price"
     t.string   "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
@@ -86,12 +85,12 @@ ActiveRecord::Schema.define(:version => 20120227162345) do
     t.string   "address"
     t.string   "city"
     t.string   "country_code"
-    t.decimal  "longitude",    :precision => 10, :scale => 0
-    t.decimal  "latitude",     :precision => 10, :scale => 0
+    t.decimal  "longitude"
+    t.decimal  "latitude"
     t.string   "phone"
     t.string   "email"
-    t.datetime "created_at",                                  :null => false
-    t.datetime "updated_at",                                  :null => false
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
     t.integer  "provider_id"
   end
 
@@ -125,9 +124,9 @@ ActiveRecord::Schema.define(:version => 20120227162345) do
     t.string   "name"
     t.string   "phone"
     t.string   "email"
-    t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
