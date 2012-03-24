ac59251b60b8bea5df1d7eaccb8c078b58fe4322
Giedrius Rimkus <info@giedriusr.lt>
2012-03-24 18:41:47 +0200

    refs #28 adding search mask in results page, fixing ip on production env

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -1,8 +1,8 @@
 class SearchResultsController < ApplicationController
-  before_filter :initialize_order
+  before_filter :initialize_order, :set_ip
 
   def index
-    @geo_location = Geocoder.search('188.4.31.200').first # request.ip
+    @geo_location = Geocoder.search(@ip).first # request.ip
 
     @search = Search.new(params[:search])
     if @search.valid?
@@ -10,6 +10,16 @@ class SearchResultsController < ApplicationController
       @items = Item.where('name LIKE ?', "%#{@search.query}%").group('provider_id')
     else
       render 'index/index'
+    end
+  end
+
+  private
+
+  def set_ip
+    if (Rails.env == 'development')
+      @ip = '188.4.31.200'
+    else
+      @ip = request.ip
     end
   end
 end
